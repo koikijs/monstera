961e0109e18ed8e431bce983c079bd1e144678c9
sideroad <sideroad.jp@gmail.com>
2016-08-29 23:01:33 +0900

    Whole calendar should be swipeable

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -75,66 +75,66 @@ export default class Calendar extends Component {
     const start = moment.utc(date).startOf('month').startOf('week');
     return (
       <div className={styles.calendar}>
-        <div className={styles.control + ' ' + styles.clearfix}>
-          <div className={styles.prev}>
-            <a className={styles.link} onClick={::this.prevMonth} href="" >
-              <div className={styles.linkcircle}></div>
-              <span><i className="fa fa-chevron-left" aria-hidden="true"></i></span>
-            </a>
-          </div>
-          <Swipeable
-            onSwipedRight={::this.prevMonth}
-            onSwipedLeft={::this.nextMonth}>
-            <div className={styles.month + ' ' + styles[className]}>
-              {date.format('MMMM') + ' ' + date.format('YYYY')}
+        <Swipeable
+          onSwipedRight={::this.prevMonth}
+          onSwipedLeft={::this.nextMonth}>
+          <div className={styles.control + ' ' + styles.clearfix}>
+            <div className={styles.prev}>
+              <a className={styles.link} onClick={::this.prevMonth} href="" >
+                <div className={styles.linkcircle}></div>
+                <span><i className="fa fa-chevron-left" aria-hidden="true"></i></span>
+              </a>
             </div>
-          </Swipeable>
-          <div className={styles.next}>
-            <a className={styles.link} onClick={::this.nextMonth} href="" >
-              <div className={styles.linkcircle}></div>
-              <span><i className="fa fa-chevron-right" aria-hidden="true"></i></span>
-            </a>
+              <div className={styles.month + ' ' + styles[className]}>
+                {date.format('MMMM') + ' ' + date.format('YYYY')}
+              </div>
+            <div className={styles.next}>
+              <a className={styles.link} onClick={::this.nextMonth} href="" >
+                <div className={styles.linkcircle}></div>
+                <span><i className="fa fa-chevron-right" aria-hidden="true"></i></span>
+              </a>
+            </div>
           </div>
-        </div>
-        <table className={styles.table + ' ' + styles[className]}>
-          <thead>
-            <tr>
-              {__.times(7, index =>
-                <th key={index} className={styles.weekday}>
-                  {moment().weekday(index).format('ddd')}
-                </th>
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {
-              __.times(5, week =>
-                <tr key={week}>
-                  {__.times(7, weekday => {
-                    const _date = moment.utc(start).add( (week * 7) + weekday, 'days');
-                    const dateClassName = [
-                                            (__.some(selected || [], item => _date.isSame(moment.utc(item).startOf('date'))) ? styles.selected : '' ),
-                                            (_date.isSame(today) ? styles.today :
-                                             __.some(holidays || [], item => _date.isSame(moment.utc(item).startOf('date'))) ? styles.holiday :
-                                            !_date.isSame(date, 'month') ? styles.other : ''),
-                                            (styles[_date.format('ddd').toLowerCase()])
-                    ].join(' ');
-                    return (<td key={weekday} className={styles.col}>
-                              <div className={dateClassName}>
-                                <div className={styles.date}>
-                                  <a className={styles.link} href="" onClick={evt => this.select(evt, _date)}>
-                                    <div className={styles.linkcircle}></div>
-                                    <span>{_date.date()}</span>
-                                  </a>
+          <table className={styles.table + ' ' + styles[className]}>
+            <thead>
+              <tr>
+                {__.times(7, index =>
+                  <th key={index} className={styles.weekday}>
+                    {moment().weekday(index).format('ddd')}
+                  </th>
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                __.times(5, week =>
+                  <tr key={week}>
+                    {__.times(7, weekday => {
+                      const _date = moment.utc(start).add( (week * 7) + weekday, 'days');
+                      const dateClassName = [
+                                              (__.some(selected || [], item => _date.isSame(moment.utc(item).startOf('date'))) ? styles.selected : '' ),
+                                              (_date.isSame(today) ? styles.today :
+                                               __.some(holidays || [], item => _date.isSame(moment.utc(item).startOf('date'))) ? styles.holiday :
+                                              !_date.isSame(date, 'month') ? styles.other : ''),
+                                              (styles[_date.format('ddd').toLowerCase()])
+                      ].join(' ');
+                      return (<td key={weekday} className={styles.col}>
+                                <div className={dateClassName}>
+                                  <div className={styles.date}>
+                                    <a className={styles.link} href="" onClick={evt => this.select(evt, _date)}>
+                                      <div className={styles.linkcircle}></div>
+                                      <span>{_date.date()}</span>
+                                    </a>
+                                  </div>
                                 </div>
-                              </div>
-                            </td>);
-                  })}
-                </tr>
-              )
-            }
-          </tbody>
-        </table>
+                              </td>);
+                    })}
+                  </tr>
+                )
+              }
+            </tbody>
+          </table>
+        </Swipeable>
       </div>
     );
   }
