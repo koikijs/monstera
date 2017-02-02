00cda3c661431aab58b1cdad2e060dd37b7e7827
sideroad <sideroad.jp@gmail.com>
2017-02-02 22:07:44 +0900

    Add no plans fields

--- src/helpers/bff.js
+++ src/helpers/bff.js
@@ -9,38 +9,56 @@ export default app => {
     const client = new ApiClient(req);
 
     client
-      .fetchJSON('https://chaus.herokuapp.com' + uris.normalize(uris.apis.event, { id: req.params.event}))
+      .fetchJSON('https://chaus.herokuapp.com' + uris.apis.attendees, 'GET', {
+        event: req.params.event,
+        limit: 10000
+      })
       .then(
-        event => {
+        attendees => {
           client
-            .fetchJSON('https://chaus.herokuapp.com' + uris.apis.candidates, 'GET', {
-              event: req.params.event,
-              date: '[' + moment().format() + ',' + moment('2999-12-31').format() + ']',
-              limit: 10000
-            })
-            .then(json => {
-              const calc = [];
-              json.items.map(item => {
-                const target = __.find(calc, {date: item.date});
-                if ( target ) {
-                  target.count++;
-                } else {
-                  calc.push({
-                    date: item.date,
-                    count: 1
-                  });
-                }
-              });
+            .fetchJSON('https://chaus.herokuapp.com' + uris.normalize(uris.apis.event, { id: req.params.event}))
+            .then(
+              event => {
+                client
+                  .fetchJSON('https://chaus.herokuapp.com' + uris.apis.candidates, 'GET', {
+                    event: req.params.event,
+                    date: '[' + moment().format() + ',' + moment('2999-12-31').format() + ']',
+                    limit: 10000
+                  })
+                  .then(json => {
+                    const calc = [];
+                    json.items.map(item => {
+                      const target = __.find(calc, {date: item.date});
+                      if ( target ) {
+                        target.count++;
+                      } else {
+                        calc.push({
+                          date: item.date,
+                          count: 1
+                        });
+                      }
+                    });
 
-              res.json(
-                __(calc)
-                  .chain()
-                  .filter(item => item.count >= event.min)
-                  .orderBy(['date'], ['asc'])
-                  .value() || []);
-            });
-        },
-        err => console.log(err)
+                    const noplans = __.reduce(json.items, (targets, candidate) => {
+                      return __.reject(targets, target => target.user.id === candidate.user.id);
+                    }, attendees.items);
+                    console.log(noplans);
+
+                    res.json({
+                      candidates:
+                        __(calc)
+                          .chain()
+                          .filter(item => item.count >= event.min)
+                          .orderBy(['date'], ['asc'])
+                          .value() || [],
+                      noplans: noplans.map( noplan => noplan.user.id )
+                    });
+
+                  });
+              },
+              err => console.log(err)
+            );
+        }
       );
   });
 };
